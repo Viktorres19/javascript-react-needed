4da4847a198f8ebcde59784faf21c4b0cb169420
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-07-17 10:42:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import FetchAxios from "./components/15-fetchAxios.jsx";
+/*import Fetch from "./components/15-fetch.jsx";*/
+/*import Forms from "./components/13-forms.jsx";*/
+/*import MySecondClassComponent from "./components/11-classesTwo.jsx";*/
+/*import MyThirdClassComponent from "./components/11-classesThree.jsx";*/
+/*import Classes from "./components/11-classes.jsx";*/
+/*import Errors from './components/12-errors.jsx'*/
+/*import Functions from './components/10-functions.jsx'*/
+/*import LoopFor from './components/09-loop-for.jsx'*/
+/*import Destructuring from './components/06-destructuring.jsx'*/
+/*import Rest from './components/07-rest.jsx'*/
+/*import Conditions from './components/08-conditions.jsx'*/
+/*import Arrays from './components/05-arrays.jsx'*/
+/*import Objects from './components/04-objects.jsx'*/
+/*import Strings from './components/03-strings.jsx'*/
+/*import VariableDeclaration from './components/01-variable-declaration.jsx'*/
+/*import DataTypes from './components/02-data-types.jsx'*/
+
+const App = () => {
+  return (
+    <>
+      {/*<VariableDeclaration />*/}
+      {/*<DataTypes />*/}
+      {/*<Strings />*/}
+      {/*<Objects />*/}
+      {/*<Arrays />*/}
+      {/*<Destructuring />*/}
+      {/*<Rest />*/}
+      {/*<Conditions />*/}
+      {/*<LoopFor />*/}
+      {/*<Functions />*/}
+      {/*<Errors />*/}
+      {/*<Classes />*/}
+      {/*<MyThirdClassComponent />*/}
+      {/*<Forms />*/}
+      {/*<Fetch />*/}
+      <FetchAxios />
+    </>
+  )
+}
+
+export default App
